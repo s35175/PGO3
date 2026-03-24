82555de8bf7c5c394f78c46b4aab9bb604d1328e
s35175 <s35175@pjwstk.edu.pl>
2026-03-24 02:15:49 +0100

    Finalize project and documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+# MiniLibrary Project
+## Project Description
+This application is a simple library management system developed for the PGO course.
+## Class List
+* Book, Reader, Library, Main
---
+++ src/Library.java
@@ -0,0 +1,74 @@
+public class Library {
+    
+    private Book[] books;
+    private int bookCount;
+
+
+    public Library(int capacity) {
+        books = new Book[capacity];
+        bookCount = 0;
+    }
+
+
+    public void addBook(Book book) {
+        if (bookCount < books.length) {
+            books[bookCount] = book;
+            bookCount++;
+            System.out.println("Added to library: " + book.getTitle());
+        } else {
+            System.out.println("Library is full!");
+        }
+    }
+
+
+    public void printAvailableBooks() {
+        System.out.println("--- Available Books ---");
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].isAvailable()) {
+                books[i].printInfo();
+            }
+        }
+    }
+
+
+    public Book findBookByTitle(String title) {
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getTitle().equalsIgnoreCase(title)) {
+                return books[i];
+            }
+        }
+        return null;
+
+
+    public int countAvailableBooks() {
+        int count = 0;
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].isAvailable()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public void borrowBook(String title, Reader reader) {
+        Book book = findBookByTitle(title);
+        if (book != null && book.isAvailable()) {
+            book.borrow();
+            reader.increaseBorrowedCount();
+            System.out.println(reader + " successfully borrowed '" + title + "'.");
+        } else {
+            System.out.println("Book is either not available or does not exist.");
+        }
+    }
+
+
+    public void returnBook(String title, Reader reader) {
+        Book book = findBookByTitle(title);
+        if (book != null && !book.isAvailable()) {
+            book.returnBook();
+            reader.decreaseBorrowedCount();
+            System.out.println("Successfully returned '" + title + "'.");
+        }
+    }
+}
